f69789976c815ae8d0342b4c4d938787340a24e0
JoeDavisRAM <ipatova.evgenija@mail.ru>
2025-06-10 21:35:25 +0300

    Update __init__.py

--- crypto-tools/src/crypto_tools/tokens/__init__.py
+++ crypto-tools/src/crypto_tools/tokens/__init__.py
@@ -7,7 +7,7 @@ Available modules:
 - jwt: JSON Web Token implementation
 - oauth: OAuth utilities
 """
-
+from .jwt_utils import create_jwt_token
 from .oauth import generate_oauth_token
 
-__all__ = ['generate_oauth_token']
+__all__ = ['generate_oauth_token', 'create_jwt_token']
